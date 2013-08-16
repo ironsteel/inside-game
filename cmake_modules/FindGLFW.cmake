46a73538945d2cab2096ed0dac6e646572757155
Rangel Ivanov <rangelivanov88@gmail.com>
2013-08-16 19:57:37 +0300

    Tweak find glfw cmake module

--- cmake_modules/FindGLFW.cmake
+++ cmake_modules/FindGLFW.cmake
@@ -20,7 +20,7 @@
 # Allow the user to select to link to a shared library or to a static library.
 
 #Search for the include file...
-FIND_PATH(GLFW_INCLUDE_DIR GL/glfw3.h DOC "Path to GLFW include directory."
+FIND_PATH(GLFW_INCLUDE_DIR GLFW/glfw3.h DOC "Path to GLFW include directory."
   HINTS
   $ENV{GLFW_ROOT}
   PATH_SUFFIX include
