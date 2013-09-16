b9e581195199460e16cd87e4a97e9d4a82ac85c6
Rangel Ivanov <rangelivanov88@gmail.com>
2013-09-16 18:22:42 +0300

    Use libpng for loading images

--- src/TextureUtils.cpp
+++ src/TextureUtils.cpp
@@ -4,55 +4,147 @@
 
 #include "TextureUtils.h"
 #include <stdio.h>
+#include <png.h>
+#include <string.h>
+#include <stdlib.h>
+
+bool TextureUtils::loadPngImage(const char *name, int &outWidth, int &outHeight, bool &outHasAlpha, GLubyte **outData) {
+	png_structp png_ptr;
+	png_infop info_ptr;
+	unsigned int sig_read = 0;
+	int color_type, interlace_type;
+	FILE *fp;
+	
+	if ((fp = fopen(name, "rb")) == NULL)
+		return false;
+	
+	/* Create and initialize the png_struct
+	 * with the desired error handler
+	 * functions.  If you want to use the
+	 * default stderr and longjump method,
+	 * you can supply NULL for the last
+	 * three parameters.  We also supply the
+	 * the compiler header file version, so
+	 * that we know if the application
+	 * was compiled with a compatible version
+	 * of the library.  REQUIRED
+	 */
+	png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING,
+									 NULL, NULL, NULL);
+	
+	if (png_ptr == NULL) {
+		fclose(fp);
+		return false;
+	}
+	
+	/* Allocate/initialize the memory
+	 * for image information.  REQUIRED. */
+	info_ptr = png_create_info_struct(png_ptr);
+	if (info_ptr == NULL) {
+		fclose(fp);
+		png_destroy_read_struct(&png_ptr, NULL, NULL);
+		return false;
+	}
+	
+	/* Set error handling if you are
+	 * using the setjmp/longjmp method
+	 * (this is the normal method of
+	 * doing things with libpng).
+	 * REQUIRED unless you  set up
+	 * your own error handlers in
+	 * the png_create_read_struct()
+	 * earlier.
+	 */
+	if (setjmp(png_jmpbuf(png_ptr))) {
+		/* Free all of the memory associated
+		 * with the png_ptr and info_ptr */
+		png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+		fclose(fp);
+		/* If we get here, we had a
+		 * problem reading the file */
+		return false;
+	}
+	
+	/* Set up the output control if
+	 * you are using standard C streams */
+	png_init_io(png_ptr, fp);
+	
+	
+	/* If we have already
+	 * read some of the signature */
+	png_set_sig_bytes(png_ptr, sig_read);
+	
+	/*
+	 * If you have enough memory to read
+	 * in the entire image at once, and
+	 * you need to specify only
+	 * transforms that can be controlled
+	 * with one of the PNG_TRANSFORM_*
+	 * bits (this presently excludes
+	 * dithering, filling, setting
+	 * background, and doing gamma
+	 * adjustment), then you can read the
+	 * entire image (including pixels)
+	 * into the info structure with this
+	 * call
+	 *
+	 * PNG_TRANSFORM_STRIP_16 |
+	 * PNG_TRANSFORM_PACKING  forces 8 bit
+	 * PNG_TRANSFORM_EXPAND forces to
+	 *  expand a palette into RGB
+	 */
+	png_read_png(png_ptr, info_ptr, PNG_TRANSFORM_STRIP_16 | PNG_TRANSFORM_PACKING | PNG_TRANSFORM_EXPAND, NULL);
+	
+	png_uint_32 width, height;
+	int bit_depth;
+	png_get_IHDR(png_ptr, info_ptr, &width, &height, &bit_depth, &color_type,
+				 &interlace_type, NULL, NULL);
+	outWidth = width;
+	outHeight = height;
+	
+	unsigned int row_bytes = png_get_rowbytes(png_ptr, info_ptr);
+	*outData = (unsigned char*) malloc(row_bytes * outHeight);
+	
+	png_bytepp row_pointers = png_get_rows(png_ptr, info_ptr);
+	
+	for (int i = 0; i < outHeight; i++) {
+		// note that png is ordered top to
+		// bottom, but OpenGL expect it bottom to top
+		// so the order or swapped
+		memcpy(*outData+(row_bytes * (outHeight-1-i)), row_pointers[i], row_bytes);
+	}
+	
+	
+	
+	/* Clean up after the read,
+	 * and free any memory allocated */
+	png_destroy_read_struct(&png_ptr, &info_ptr, NULL);
+	
+	/* Close the file */
+	fclose(fp);
+	
+	/* That's it */
+	return true;
+}
 
 void TextureUtils::loadTexture(const char* textName, GLuint *g_textureID)
 {
-	/////////////////////////////////////////////
-	// NEW! - This function has been completely
-	// rewritten to use FreeImage.
+	GLubyte *imageData;
 	
+	int width;
+	int height;
 	
-	// Get the image file type from FreeImage.
-	FREE_IMAGE_FORMAT fifmt = FreeImage_GetFileType(textName, 0);
+	bool hasAlpha;
 	
-	// Actually load the image file.
-	FIBITMAP *dib = FreeImage_Load(fifmt, textName,0);
-	// Now, there is no guarantee that the image file
-	// loaded will be GL_RGB, so we force FreeImage to
-	// convert the image to GL_RGB.
-	dib = FreeImage_ConvertTo24Bits(dib);
+	TextureUtils::loadPngImage(textName, width, height, hasAlpha, &imageData);
 	
-	if( dib != NULL )
-	{
-		glGenTextures(1, g_textureID);
-		glBindTexture( GL_TEXTURE_2D, *g_textureID );
-		glTexParameteri( GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		glTexParameteri( GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-		
-		// This is important to note, FreeImage loads textures in
-		// BGR format. Now we could just use the GL_BGR extension
-		// But, we will simply swap the B and R components ourselves.
-		// Firstly, allocate the new bit data doe the image.
-		BYTE *bits = new BYTE[FreeImage_GetWidth(dib) * FreeImage_GetHeight(dib) * 3];
-		// get a pointer to FreeImage's data.
-		BYTE *pixels = (BYTE*)FreeImage_GetBits(dib);
-		// Iterate through the pixels, copying the data
-		// from 'pixels' to 'bits' except in RGB format.
-		for(int pix=0; pix<FreeImage_GetWidth(dib) * FreeImage_GetHeight(dib); pix++)
-		{
-			bits[pix*3+0]=pixels[pix*3+2];
-			bits[pix*3+1]=pixels[pix*3+1];
-			bits[pix*3+2]=pixels[pix*3+0];
-		}
-		// The new 'glTexImage2D' function, the prime difference
-		// being that it gets the width, height and pixel information
-		// from 'bits', which is the RGB pixel data..
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, FreeImage_GetWidth(dib), FreeImage_GetHeight(dib), 0,
-					  GL_RGB, GL_UNSIGNED_BYTE, bits);
-		glGenerateMipmap(GL_TEXTURE_2D);
-		// Unload the image.
-		// and free the bit data.
-		FreeImage_Unload(dib);
-		delete[] bits;
-	}
+	glGenTextures(1, g_textureID);
+	glBindTexture( GL_TEXTURE_2D, *g_textureID );
+	glTexParameteri( GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTexParameteri( GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, imageData);
+	glGenerateMipmap(GL_TEXTURE_2D);
+	
+
 }
