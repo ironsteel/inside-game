cf7b9f3343761ff75aa6b758af052f1906daa58d
Rangel Ivanov <rangelivanov88@gmail.com>
2014-01-05 15:52:37 +0200

    Minor changes to license header

--- include/Camera.h
+++ include/Camera.h
@@ -1,6 +1,6 @@
 /*
  * Inside Game
- * Copyright (C) 2013  Rangel Ivanov rangelivanov88[at]gmail.com
+ * Copyright (C) 2013  Rangel Ivanov <rangelivanov88[at]gmail.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
